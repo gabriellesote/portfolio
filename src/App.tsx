b30ba0652ae0980b65561f9bc57ea4bbcbd8cae1
gabrielle <gabrielle.sote@gmail.com>
2025-08-14 23:44:05 -0300

    -ajustes do scroll, coloquei 3 seções. - construção do card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
-import Card from "./components/Card"
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="bg-background-50 h-screen">
-     <Card/>
+    <div
+    className="
+    h-screen overflow-y-scroll snap-y snap-mandatory
+    overflow-auto bg-background-50"
+    >
+      <section className="h-screen snap-start bg-red-500">
+        <h1> Intro</h1>
+         
+       
+      </section>
+
+      <section className="h-screen snap-start bg-green-500 ">
+        <h1>Projetos</h1>
+        <div className="bg-accent-50 
+        
+        table ">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+       
+        
+        </div>
+      </section>
+
+      <section className="h-screen snap-start bg-blue-500">
+        <h3>contato</h3>
+     
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,33 @@
-function Card(){
-    return(
-        <section className="bg-background-100 h-fit w-3xs">
+function Card() {
+  return (
+    <section
+    className="
+    bg-secondary-100 h-max w-[20rem] 
+    box-border rounded-[1rem] p-[1rem] 
+    flex flex-col justify-center items-center 
+    border-2 border-accent-800 m-[1rem]"
+    >
+      <header className="text-text-800">
+        <h1 className="text-3xl">Titulo do projeto</h1>
+        <h3> descrição (api, ou qualquer coisa)</h3>
+      </header>
+      <div className="flex flex-col justify-center items-center">
+        <img
+          src="/src/assets/images/cat2.jpg"
+          alt="placeholder cat"
+          className="h-30 w-200 rounded-[1rem]"
+        />
+      </div>
 
-            <p>teste</p>
-        </section>
-    )
+      <footer className="text-text-900 text-justify"> 
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit in odit, eum, excepturi ipsa labore itaque, quia minus
+          bl
+        </p>
+      </footer>
+    </section>
+  );
 }
 
 export default Card;
