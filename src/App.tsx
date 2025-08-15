0de02dede5c902b245bfb9c82b43d03ea7748f03
gabrielle <gabrielle.sote@gmail.com>
2025-08-14 21:51:46 -0300

    inicio de estilo do card

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Card from "./components/Card"
+
+function App() {
+  return (
+    <div className="bg-background-50 h-screen">
+     <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.tsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <section className="bg-background-100 h-fit w-3xs">
+
+            <p>teste</p>
+        </section>
+    )
+}
+
+export default Card;
